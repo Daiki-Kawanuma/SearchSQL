95a5643088f41babe39e5cd6d83e96690eaad549
Daiki <daiki.kawanuma@gmail.com>
2016-10-08 17:42:21 +0900

    adjust UserResultPgae

---
+++ TwitterSQL/TwitterSQL/Converters/DateConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TwitterSQL.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTimeOffset)
+            {
+                var dateTimeOffset = value as DateTimeOffset? ?? new DateTimeOffset();
+                return dateTimeOffset.LocalDateTime.ToString("yyyy/MM/dd");
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
